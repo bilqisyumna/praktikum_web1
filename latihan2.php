466a7021b7a56fad2f9f1109a77b1ae178e86f74
aufannidaaz <afndaazukhruf@gmail.com>
2026-04-24 13:59:33 +0700

    belajar php

---
+++ latihan2.php
@@ -0,0 +1,10 @@
+<?php
+$nim = '1202407017';
+$nama = 'Bilqis Yumna Alamsyah';
+$alamat = 'Jl. Kalingga 7 no. 14 rt.06/rw.02';
+$nilai = 90;
+echo "NIM : ", $nim, "<br>";
+echo "Nama : ", $nama, "<br>";
+echo "Alamat : ", $alamat, "<br>";
+echo "Nilai : $nilai";
+?>
